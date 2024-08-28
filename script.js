a263a8cb2eab9b323999f9f5252b1ae51b12ccdc
alma88gc <alde88gc@gmail.com>
2024-08-27 22:58:26 -0500

    Adelanto Encriptador de texto

---
+++ script.js
@@ -0,0 +1,58 @@
+//datos
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const btn_copiar = document.querySelector("btn-copiar")
+const copiar = document.querySelector(".mensaje");
+//La letra "e" = "enter"
+//La letra "i" = "imes"
+//La letra "a" = "ai"
+//La letra "o" = "ober"
+//La letra "u" = "ufat"
+
+
+//boton encriptar
+function btnEncriptar(){
+  const textoEncriptado = encriptar(textArea.value)
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada){
+let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+stringEncriptada = stringEncriptada.toLowerCase()
+      
+for(let i=0; i<matrizCodigo.length; i++){
+      if(stringEncriptada.includes(matrizCodigo[i][0])){
+        stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+
+      }
+
+}
+return stringEncriptada;
+}
+
+//boton desencriptar
+function btnDesencriptar(){
+  const textoEncriptado = desencriptar(textArea.value)
+  mensaje.value = textoEncriptado;
+  textArea.value = "";
+ 
+}
+
+function desencriptar(stringDesencriptada){
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  stringDesencriptada = stringDesencriptada.toLowerCase()
+        
+  for(let i=0; i<matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+          stringDesencriptada=stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+  
+  
+        }
+  
+  }
+  return stringDesencriptada;
+}
+
